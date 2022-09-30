2ed96a746be12b0c9b103c876583e51f7285647c
EkeMinusYou <euonymuslke@gmail.com>
2022-09-30 14:28:41 +0900

    feat unfurl logic

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module golang-template
 
 go 1.19
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.11.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/google/uuid"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 )
 
 var (
@@ -18,7 +24,55 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
+	client := slack.New(SlackToken, slack.OptionDebug(true))
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalf("failed to read request body: %s", err.Error())
+	}
+	event, err := slackevents.ParseEvent(
+		json.RawMessage(body),
+		slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: VerificationToken}),
+	)
+	if err != nil {
+		log.Fatalf("failed to parse event: %s", err.Error())
+	}
+
+	linkSharedEvent, ok := event.InnerEvent.Data.(*slackevents.LinkSharedEvent)
+	if !ok {
+		log.Fatal("event is not LinkSharedEvent")
+	}
+	link := linkSharedEvent.Links[0]
+
+	externalId := uuid.NewString()
+	remoteFile, err := client.AddRemoteFile(slack.RemoteFileParameters{
+		ExternalID:   "slack unfurl test: " + externalId,
+		ExternalURL:  "http://example.com",
+		Title:        "slack unfurl test",
+		PreviewImage: "https://go.dev/images/gophers/ladder.svg",
+	})
+	if err != nil {
+		log.Fatalf("failed to add remote file. err=%s", err)
+	}
+	fmt.Println(remoteFile.ExternalID)
+
+	blocks := make([]slack.Block, 0, 1)
+	blocks = append(blocks, slack.NewFileBlock("", remoteFile.ExternalID, "remote"))
+	_, _, _, err = client.UnfurlMessage(
+		linkSharedEvent.Channel,
+		linkSharedEvent.TimeStamp,
+		map[string]slack.Attachment{
+			link.URL: {
+				Blocks: slack.Blocks{
+					BlockSet: blocks,
+				},
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalf("failed to unfurl remote file. URL=%s err=%s", link.URL, err)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
